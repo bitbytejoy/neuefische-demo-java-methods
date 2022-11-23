50910a62fb75d5adc8274f922c4017c753286942
bitbytejoy <bitbytejoy@gmail.com>
2022-11-23 10:26:49 +0100

    init: BIG BANG

---
+++ Main.java
@@ -0,0 +1,44 @@
+public class Main {
+    public static void main (String[] args) {
+        int a = 10;
+        int b = 5;
+
+        int c = doubleTheNumber(a);
+        int d = doubleTheNumber(b);
+
+        System.out.println(c);
+        System.out.println(d);
+
+        helloWorld();
+        helloWorld();
+
+        String greeting1 = greet("Doreen");
+        String greeting2 = greet("Christian");
+
+        System.out.println(greeting1);
+        System.out.println(greeting2);
+    }
+
+    public static int doubleTheNumber(int number) {
+        return number * 2;
+    }
+
+    public static String greet(String name) {
+        return "Hello " + name;
+    }
+
+    public static int sumTheNumbers (int number1, int number2, int number3) {
+        return number1 + number2 + number3;
+    }
+
+    public static void greet2Persons (String name1, String name2) {
+        System.out.println("Hello " + name1);
+        System.out.println("Hello " + name2);
+    }
+
+    public static void helloWorld () {
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+    }
+}
